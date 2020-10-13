50af4b0e58b03cc9086b5e6713c3234533864e55
thanhbk55 <thanhdd1992@gmail.com>
2020-10-13 08:06:06 +0700

    fetch data

--- pages/api/pages/[id].js
+++ pages/api/pages/[id].js
@@ -1,6 +1,7 @@
 import middleware from '../../../database';
 
 middleware.get(async (req, res) => {
+  console.log(req.query)
   let doc = await req.db.collection('pages').findOne({"name": req.query.id})
   res.json(doc);
 });
---
+++ pages/pages/[id].js
@@ -0,0 +1,55 @@
+export default function Page({page}) {
+  return (
+    <div>
+      <h2>{page.name}</h2>
+      <p>{page.content}</p>
+    </div>
+  )
+}
+
+Page.getInitialProps = async ({query}) => {
+  const url =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "https://cms-refactor-nextjs-app.herokuapp.com";
+  const res = await fetch(`${url}/api/pages/${query.id}`);
+  const json = await res.json();
+  
+  return {
+    page: json
+  };
+}
+
+// export async function getStaticPaths() {
+//   const url =
+//     process.env.NODE_ENV === "development"
+//       ? "http://localhost:3000"
+//       : "https://cms-refactor-nextjs-app.herokuapp.com";
+//   const res = await fetch(`${url}/api/all`);
+//   const json = await res.json();
+
+//   const paths = json.map((j) => ({
+//     params: { id: j.name },
+//   }))
+
+//   // const paths = [{name: "page0"},{name: "page1"},{name: "page2"},{name: "page3"},{name: "page4"},{name: "page5"}].map((j) => ({
+//   //   params: { id: j.name },
+//   // }))
+
+//   return { paths, fallback: false }
+// }
+
+// export async function getStaticProps({params}) {
+//   const url =
+//     process.env.NODE_ENV === "development"
+//       ? "http://localhost:3000"
+//       : "https://cms-refactor-nextjs-app.herokuapp.com";
+//   const res = await fetch(`${url}/api/pages/${params.id}`);
+//   const json = await res.json();
+  
+//   return {
+//     props: {
+//       page: json
+//     }
+//   };
+// }
