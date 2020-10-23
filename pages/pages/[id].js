ba95ce4b4176e9129711bae200170dafc9911f9e
quang duy <quangduybk95@gmail.com>
2020-10-23 12:13:05 +0900

    demo

--- pages/pages/[id].js
+++ pages/pages/[id].js
@@ -1,55 +1,58 @@
-export default function Page({page}) {
+import axios from 'axios'
+
+export default function Page({page, cookie_query}) {
   return (
     <div>
       <h2>{page.name}</h2>
-      <p>{page.content}</p>
+      <h2>cookie</h2>
+      <p style={{wordBreak: "break-word"}}>{cookie_query.input_cookie}</p>
+      <h2>authen_status</h2>
+      <p>{cookie_query.status}</p>
+      <h2>authen_response_body</h2>
+      <p>{cookie_query.authen_body && JSON.stringify(cookie_query.authen_body)}</p>
     </div>
   )
 }
 
-Page.getInitialProps = async ({query}) => {
+Page.getInitialProps = async (ctx) => {
+  const {cookie} = ctx.req.headers
+
+  const response = await axios({
+    method: 'get',
+    url: `http://homeup.local:3000/check_auth_cms?format=json&client_id=c18112020900&method=abm_ips`,
+    headers: {Cookie: cookie}
+  })
+
+  const {query} = ctx
   const url =
     process.env.NODE_ENV === "development"
-      ? "http://localhost:3000"
+      ? "http://homeup.local:9000/new_cms"
       : "https://cms-refactor-nextjs-app.herokuapp.com";
   const res = await fetch(`${url}/api/pages/${query.id}`);
   const json = await res.json();
-  
+  console.log(response.status)
   return {
-    page: json
+    page: json,
+    cookie_query: {
+      status: response.status,
+      input_cookie: cookie,
+      authen_body: response.data
+    }
   };
 }
 
-// export async function getStaticPaths() {
-//   const url =
-//     process.env.NODE_ENV === "development"
-//       ? "http://localhost:3000"
-//       : "https://cms-refactor-nextjs-app.herokuapp.com";
-//   const res = await fetch(`${url}/api/all`);
-//   const json = await res.json();
-
-//   const paths = json.map((j) => ({
-//     params: { id: j.name },
-//   }))
-
-//   // const paths = [{name: "page0"},{name: "page1"},{name: "page2"},{name: "page3"},{name: "page4"},{name: "page5"}].map((j) => ({
-//   //   params: { id: j.name },
-//   // }))
-
-//   return { paths, fallback: false }
-// }
-
-// export async function getStaticProps({params}) {
-//   const url =
-//     process.env.NODE_ENV === "development"
-//       ? "http://localhost:3000"
-//       : "https://cms-refactor-nextjs-app.herokuapp.com";
-//   const res = await fetch(`${url}/api/pages/${params.id}`);
-//   const json = await res.json();
-  
-//   return {
-//     props: {
-//       page: json
-//     }
-//   };
-// }
+export function getCookie(cookie, cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
