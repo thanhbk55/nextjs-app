7fbd198f45f5198217b1619b3a847fed810e57bf
Thanh <thanhdd1992@gmail.com>
2020-11-14 01:44:13 +0900

    change database

--- database.js
+++ database.js
@@ -1,7 +1,8 @@
 import { MongoClient } from 'mongodb';
 import nextConnect from 'next-connect';
+// mongodb+srv://thanhbk55:thanh1992@cluster0.tnjxh.mongodb.net/test?retryWrites=true&w=majority
 
-const client = new MongoClient('mongodb://uri0lh8bozwmlvwh5pbz:zxnN3slU8a3LTYMoy5nC@bibbdgl6jerkqlu-mongodb.services.clever-cloud.com:27017/bibbdgl6jerkqlu', {
+const client = new MongoClient('mongodb+srv://thanhbk55:thanh1992@cluster0.tnjxh.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -9,7 +10,7 @@ const client = new MongoClient('mongodb://uri0lh8bozwmlvwh5pbz:zxnN3slU8a3LTYMoy
 async function database(req, res, next) {
   if (!client.isConnected()) await client.connect();
   req.dbClient = client;
-  req.db = client.db('bibbdgl6jerkqlu');
+  req.db = client.db('test');
   return next();
 }
 
