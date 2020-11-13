7c9b93a5f070fd9fe8c033a9c804dbf9dab367e5
Thanh <thanhdd1992@gmail.com>
2020-11-14 03:43:22 +0900

    fix api

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ import { MongoClient } from 'mongodb';
 import nextConnect from 'next-connect';
 // mongodb+srv://thanhbk55:thanh1992@cluster0.tnjxh.mongodb.net/test?retryWrites=true&w=majority
 
-const client = new MongoClient('mongodb+srv://thanhbk55:thanh1992@cluster0.tnjxh.mongodb.net/test?retryWrites=true&w=majority', {
+const client = new MongoClient('mongodb://uri0lh8bozwmlvwh5pbz:zxnN3slU8a3LTYMoy5nC@bibbdgl6jerkqlu-mongodb.services.clever-cloud.com:27017/bibbdgl6jerkqlu', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -10,7 +10,7 @@ const client = new MongoClient('mongodb+srv://thanhbk55:thanh1992@cluster0.tnjxh
 async function database(req, res, next) {
   if (!client.isConnected()) await client.connect();
   req.dbClient = client;
-  req.db = client.db('test');
+  req.db = client.db('bibbdgl6jerkqlu');
   return next();
 }
 
--- pages/api/pages/[id].js
+++ pages/api/pages/[id].js
@@ -1,8 +1,8 @@
 import middleware from '../../../database';
 
 middleware.get(async (req, res) => {
-  let doc = await req.db.collection('page_test').findOne({"name": req.query.id})
-  res.json(doc);
+  let doc = await req.db.collection('pages').findOne({"name": req.query.id})
+  return res.json(doc);
 });
 
 export default middleware;
