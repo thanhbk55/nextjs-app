d05566cf92259f098940ad870c6537f7bef3d3b4
Thanh <thanhdd1992@gmail.com>
2020-11-13 23:58:07 +0900

    test static

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
-import Head from 'next/head'
+// import Head from 'next/head'
 
 export default function Home() {
 
   return (
     <div>
       CMS
+      <br/>
+      <a href="/pages/page1">P1</a><br/>
+      <a href="/pages/page2">P2</a><br/>
+      <a href="/pages/page3">P3</a><br/>
+      <a href="/pages/page4">P4</a>
     </div>
   )
 }
